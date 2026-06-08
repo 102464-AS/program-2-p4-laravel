95a2414f854d868d6b1515e10949b4fb8796f06f
André Smalheer <102464@glr.nl>
2026-06-08 09:02:48 +0000

    opdracht 3.2 relatie tussen modellen

--- database/migrations/2026_06_08_085729_create_evenement_types_table.php
+++ database/migrations/2026_06_08_085729_create_evenement_types_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('evenements', function (Blueprint $table) {
-            $table->foreignId('evenement_type_id')->constrained()->onDelete('cascade');
+        Schema::create('evenement_types', function (Blueprint $table) {
             $table->id();
             $table->string('naam');
-            $table->text('beschrijving')->nullable();
-            $table->date('datum');
-            $table->string('locatie');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('evenements');
+        Schema::dropIfExists('evenement_types');
     }
 };
