b323412492d0bb2399605df28c9cd02a3740e2d1
André Smalheer <102464@glr.nl>
2026-06-08 08:56:33 +0000

    opdracht 3:1 model en megratie aanmake

---
+++ database/migrations/2026_06_08_085421_create_evenements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+            $table->id();
+            $table->string('naam');
+            $table->text('beschrijving')->nullable();
+            $table->date('datum');
+            $table->string('locatie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evenements');
+    }
+};
